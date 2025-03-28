e5997a1412c32e3ef57f4a2bdd9fd159705adf96
paragkoche <paragkoche0@gmail.com>
2025-03-28 10:39:22 +0530

    feat: add footer component with logo and copyright information

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import Header from "@/components/layout/headers/header";
+import Footer from "@/components/layout/footer/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className={`antialiased`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/components/layout/headers/header.tsx
+++ src/components/layout/headers/header.tsx
@@ -11,7 +11,7 @@ const Header = async () => {
   console.log(data, error);
 
   return (
-    <header className="border-b border-b-[rgba(0,0,0,.1)] z-20 max-h-16 uppercase py-4 relative px-5 sm:py-4">
+    <header className="border-b border-b-[rgba(0,0,0,.1)] z-20 max-h-16 uppercase py-4 relative px-3.5 sm:py-4">
       <div className="flex justify-between items-center    max-w-[1336px] mx-auto ">
         <div className="hidden sm:block">
           <nav className="flex gap-9">
